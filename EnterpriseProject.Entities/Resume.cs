004358a9e5757175d4d1a90e7392603a3f4bb415
duttasah <duttasah@sheridancollege.ca>
2025-01-02 18:16:23 -0500

    Add Entites

---
+++ EnterpriseProject.Entities/Resume.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EnterpriseProject.Entities
+{
+    public class Resume
+    {
+        [Key]
+        public int ResumeId { get; set; }
+
+        [Required]
+        public string FilePath { get; set; } = String.Empty;
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ EnterpriseProject.Entities/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseProject.Entities
+{
+    public class User
+    {
+        public const string ClaimType = "UserId";
+
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        
+        public string UserName { get; set; } = String.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; } = String.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = String.Empty;
+
+        // Navigation Properties
+        public List<Project> Projects { get; set; } = new List<Project>();
+        public Resume? Resume { get; set; }
+        public Profile Profile { get; set; } = new Profile();
+
+        public List<Comment> Comments { get; set; }
+    }
+}
